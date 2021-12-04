15ce4e51d0f4ff7f1013bdd74b0b9978ecf4a299
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-12-04 15:57:30 -0300

    Ensure TokenGenerator calls JWT with correct values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,6 +1,8 @@
 module.exports = {
   token: 'any_token',
   sign (payload, secretOrPrivateKey) {
+    this.payload = payload
+    this.secretOrPrivateKey = secretOrPrivateKey
     return this.token
   }
 }
