462fd80e8eef97a8b0e45724df9b1e5eb960926b
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-12-05 15:49:44 -0300

    Add express router adapter

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,5 +1,6 @@
 const loginRouter = require('../composers/login-router-compose')
+const ExpressRouterAdapter = require('../adapters/express-router-adapter')
 
 module.exports = router => {
-  router.post('/login', loginRouter)
+  router.post('/login', ExpressRouterAdapter.adapt(loginRouter))
 }
