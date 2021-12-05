aa54410347d48edffefe2241e8c27c50858cee0a
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-12-05 17:16:06 -0300

    Add login route integration test ensure 401 response when invalid credentials are provided

--- src/main/routes/login-router.test.js
+++ src/main/routes/login-router.test.js
@@ -29,4 +29,11 @@ describe('LoginRouter', () => {
       password: 'hashed_password'
     }).expect(200)
   })
+
+  test('Should return 401 when invalid credentials are provided', async () => {
+    await request(app).post('/api/login').send({
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    }).expect(401)
+  })
 })
