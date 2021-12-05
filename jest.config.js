cd865315a6486813f4acfe10cd8d4b0ab0ae75dd
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-12-05 09:34:43 -0300

    Ignore module main in coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   collectCoverage: false,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   preset: '@shelf/jest-mongodb'
 }
